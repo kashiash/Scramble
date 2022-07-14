e446eee3dbcd3bc40dc26561ca874984d4c7bb2b
kashiash <kashiash@gmail.com>
2022-07-14 21:33:08 +0200

    Initial Commit

---
+++ Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Jacek Placek on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
